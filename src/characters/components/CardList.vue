07c875f4b406f62c88049f4de69892cbf01adde6
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-12-19 20:29:40 -0600

    feat: tanstack query configuration}

--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -4,14 +4,21 @@ import { ref } from "vue";
 import rickAndMortyApi from "@/api/rickAndMortyApi";
 import type { Character, ResponseCharacter } from "../../interfaces/character";
 import { useCharacters } from "@/characters/composables";
+import { useQuery } from "@tanstack/vue-query";
 
 // useCharacters with global state
-const { characters, isLoading, hasError, errorMessage } = useCharacters();
+// const { characters, isLoading, hasError, errorMessage } = useCharacters();
+const getCharactersSlow = async (): Promise<Character[]> => {
+    const { data } = await rickAndMortyApi.get<ResponseCharacter>("/character");
+    return data.results;
+};
+
+const { isError, isLoading, error, data: characters } = useQuery(["characters"], getCharactersSlow);
 </script>
 <template>
     <h1 v-if="isLoading">Loading...</h1>
-    <template v-if="hasError">
-        <h1>{{ errorMessage }}</h1>
+    <template v-if="isError">
+        <h1>{{ error }}</h1>
     </template>
     <ul>
         <li v-for="character in characters" :key="character.id">
