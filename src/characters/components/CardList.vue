d971e805644dd7f738b07199389d6987757ec047
Alejandro Dubon <alejandrodubon88@gmail.com>
2022-12-19 20:37:01 -0600

    feat: useQuery options

--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -9,11 +9,23 @@ import { useQuery } from "@tanstack/vue-query";
 // useCharacters with global state
 // const { characters, isLoading, hasError, errorMessage } = useCharacters();
 const getCharactersSlow = async (): Promise<Character[]> => {
-    const { data } = await rickAndMortyApi.get<ResponseCharacter>("/character");
-    return data.results;
+    return new Promise((resolve, reject) => {
+        setTimeout(async () => {
+            const { data } = await rickAndMortyApi.get<ResponseCharacter>("/character");
+            resolve(data.results);
+        }, 3000);
+    });
 };
 
-const { isError, isLoading, error, data: characters } = useQuery(["characters"], getCharactersSlow);
+const {
+    isError,
+    isLoading,
+    error,
+    data: characters,
+} = useQuery(["characters"], getCharactersSlow, {
+    cacheTime: 3000 * 60,
+    refetchOnReconnect: 'always'
+});
 </script>
 <template>
     <h1 v-if="isLoading">Loading...</h1>
